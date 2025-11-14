d7c374759e6332310409f026bb18b338201762f9
Almas <a.tazhiyev@smartderek.kz>
2025-11-14 18:13:13 +0500

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Project invest SKO ReactTS
+
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { BrowserRouter } from "react-router";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <h1>Invest SKO</h1>
+    </BrowserRouter>
+  );
+};
+
+export default App;
