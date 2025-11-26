610100553ec0bfa973cab595f5327d9bbfd082bf
Almas <a.tazhiyev@smartderek.kz>
2025-11-26 10:57:57 +0500

    add default lang

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -37,7 +37,7 @@ const App = () => {
         <Routes>
           <Route
             path="/"
-            element={<Navigate to={`${useLang.lang}`} replace />}
+            element={<Navigate to={`/ru`} replace />}
           />
 
           {routeList.map((route, index) => (
