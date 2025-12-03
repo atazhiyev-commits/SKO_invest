b8057fef481c4b224664514b3e2fca1802fc60cb
Almas <a.tazhiyev@smartderek.kz>
2025-12-03 16:48:44 +0500

    add banks

--- src/pages/forInvestor/banks.tsx
+++ src/pages/forInvestor/banks.tsx
@@ -1,5 +1,10 @@
-import clsx from 'clsx';
-import { type FC } from 'react';
+import clsx from "clsx";
+import { type FC } from "react";
+import Markdown from "react-markdown";
+import textMD from "./../MD.md?raw";
+import { Landmark } from "lucide-react";
+
+import "./forinvestor.scss"
 
 interface Props {
   className?: string;
@@ -7,9 +12,13 @@ interface Props {
 
 const Banks: FC<Props> = ({ className }) => {
   return (
-    <>
-    <h2 className={clsx("catalog__banks titleInvest", className)}>Banks</h2>
-    </>
+    <div className="bank-page">
+      <h2 className={clsx("bank-page__title", className)}>Banks</h2>
+      <Landmark size={"256"} style={{ marginBlock: "36px" }} />
+      <div className="bank-page__content">
+        <Markdown>{textMD}</Markdown>
+      </div>
+    </div>
   );
 };
 
